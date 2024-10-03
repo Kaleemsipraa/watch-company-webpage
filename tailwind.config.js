a4bb9f22ca24bb0c57afecad41ad36b2b271c745
KaleemSipraa <kaleemsipraa@gmail.com>
2024-10-03 22:09:57 +0500

    navbar home section and mens collection section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#1f1f1f",
+        "primary-color-light": "#7f7e7e",
+        "secondary-color": "#f89b1a",
+        "secondary-dark-color": "#d27b02"
+      },
+    },
   },
   plugins: [],
-}
-
+};
